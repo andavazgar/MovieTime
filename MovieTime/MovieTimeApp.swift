f942e6b1e2100cb89cd21fbaff0e7cf5a2c987f8
Andres Vazquez <andavazgar.dev@gmail.com>
2023-02-06 22:01:13 -0500

    Initial Commit

---
+++ MovieTime/MovieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTimeApp.swift
+//  MovieTime
+//
+//  Created by Andres Vazquez on 2023-02-06.
+//
+
+import SwiftUI
+
+@main
+struct MovieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
